9e17069bf1d6787fd1de4cb17bf86fd64e0bc648
victorlarac <49878618+victorlarac@users.noreply.github.com>
2022-02-15 16:47:58 -0300

    Update index.js

--- back/src/api/index.js
+++ back/src/api/index.js
@@ -5,9 +5,11 @@ import auth from './auth'
 import endereco from './enderecos'
 import funcionario from './funcionario'
 import paciente from './paciente'
+import agendamento from './agendamento'
 
 const router = new Router()
 
+router.use('/agendamento', agendamento)
 router.use('/paciente', paciente)
 router.use('/funcionario', funcionario)
 router.use('/endereco', endereco)
